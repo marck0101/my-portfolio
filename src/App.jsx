1a8671c47dcc4ba767f72d2235f9fa1cb2e09a3e
Marcos <marcos.henrique.a7x@@gmail.com>
2023-02-12 11:41:58 -0300

    init portfolio

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import Footer from "./components/footer/footer";
+import Header from "./components/Header/Header";
+
+
+function App() {
+  return (
+    <div className="">
+      <header className="App-header">
+        <Header />
+        <Footer />
+      </header>
+    </div>
+  );
+}
+
+export default App;
